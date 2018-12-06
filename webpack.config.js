cea2996826b738b13b524a2002c09005b60d4d64
Bryan Weber <bryan.weber@looker.com>
2018-12-06 13:51:03 -0800

    added configs

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    https: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+};
