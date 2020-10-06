042cf8d7fa0798c2e5542c56ee1bc2b89beeec09
Elliot Glasenk <eglasenk@gmail.com>
2020-10-06 15:24:04 -0400

    Comment out other sidebar elements for now

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Box, StyledMenuItemProps, Heading, MenuList, Icon, Text } from '@looker/components'
-import  React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link as RouterLink, LinkProps } from 'react-router-dom'
 import styled from 'styled-components'
 import omit from 'lodash/omit'
@@ -15,28 +15,28 @@ import { ExtensionContextData, ExtensionContext } from '@looker/extension-sdk-re
 import { ExtensionHostApi } from '@looker/extension-sdk'
 
 export const Sidebar: React.FC<any> = ({ selection, last_selection, refresh_qid, refresh_did, refresh_sql, refresh_lid, resetSidebarNotification }) => {
-  const {editing, setAppParams} = useContext(AppContext)
+  const { editing, setAppParams } = useContext(AppContext)
   const extensionContext = useContext<ExtensionContextData>(ExtensionContext)
   const extensionHost = extensionContext.extensionSDK as ExtensionHostApi
 
   const updateSelection = (route) => {
     resetSidebarNotification(route)
-    extensionHost.track('click','sql-runner-tracked', {selection: route, editing})
-    setAppParams({selection: route})
+    extensionHost.track('click', 'sql-runner-tracked', { selection: route, editing })
+    setAppParams({ selection: route })
   }
 
-  const SidebarMenuItem: any = ({children, refresh, route, ...props}: any) => {
+  const SidebarMenuItem: any = ({ children, refresh, route, ...props }: any) => {
     const [start, setStart] = useState(false)
 
     useEffect(() => {
-      if (refresh > 0) { 
-        setStart(true) 
+      if (refresh > 0) {
+        setStart(true)
       }
     }, [refresh])
-    
-    useEffect(()=>{
+
+    useEffect(() => {
       if (start) {
-        const timer = setTimeout(() => { 
+        const timer = setTimeout(() => {
           resetSidebarNotification(route)
           setStart(false)
         }, 6000);
@@ -47,8 +47,8 @@ export const Sidebar: React.FC<any> = ({ selection, last_selection, refresh_qid,
     return <StyledMenuItem
       {...props}
       key={`${route}::${refresh}`}
-      animate={(start)? true : false}
-      onClick={()=>{updateSelection(route)}}
+      animate={(start) ? true : false}
+      onClick={() => { updateSelection(route) }}
       current={selection === route}
     >
       {children}
@@ -57,30 +57,30 @@ export const Sidebar: React.FC<any> = ({ selection, last_selection, refresh_qid,
 
 
   return (
-    <Box 
-      display="flex" 
+    <Box
+      display="flex"
       flexDirection="column"
       pt="large"
     >
-      <Heading 
+      <Heading
         pl="small"
         mb="small"
         fontSize="large"
       >SQL Explorer <Text fontSize="xxsmall">(alpha)</Text></Heading>
-        <SidebarMenuItem
-          route={ROUTES.EMBED_SQL}
-          refresh={refresh_sql}
-          icon="SqlRunner"
-        >
-          Write SQL
+      <SidebarMenuItem
+        route={ROUTES.EMBED_SQL}
+        refresh={refresh_sql}
+        icon="SqlRunner"
+      >
+        Write SQL
           </SidebarMenuItem>
-      {(selection === ROUTES.EMBED_SQL) && <SidebarSql /> }
-        <SidebarMenuItem
-            route={ROUTES.EMBED_EXPLORE}
-            refresh={refresh_qid}
-            icon="Explore"
-          >
-            Explore SQL
+      {/* {(selection === ROUTES.EMBED_SQL) && <SidebarSql />}
+      <SidebarMenuItem
+        route={ROUTES.EMBED_EXPLORE}
+        refresh={refresh_qid}
+        icon="Explore"
+      >
+        Explore SQL
           </SidebarMenuItem>
       {(selection === ROUTES.EMBED_EXPLORE) && <SidebarExplore/> }
       <SidebarMenuItem
@@ -106,7 +106,7 @@ export const Sidebar: React.FC<any> = ({ selection, last_selection, refresh_qid,
             icon="Help"
           >
             Help
-          </SidebarMenuItem>
+          </SidebarMenuItem> */}
     </Box>
   )
 }
