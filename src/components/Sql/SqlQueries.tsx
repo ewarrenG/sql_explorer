625d9c14a5bad4580cb05ee3a3190439a30f0510
Elliot Glasenk <eglasenk@gmail.com>
2020-10-26 17:39:47 -0400

    Refactor date range query modification into single helper function

--- src/components/Sql/SqlQueries.tsx
+++ src/components/Sql/SqlQueries.tsx
@@ -17,15 +17,15 @@ import { SqlSidebarColumns } from './Sidebar/SqlSidebarColumns';
 import { SqlSidebarRun } from './Sidebar/SqlSidebarRun';
 import { find } from 'lodash';
 import AppContext from '../../AppContext';
-
-console.log({ InputDateRange })
-
+/**
+ * to do 
+ * implement min max logic to date range input
+ */
 export function SqlQueries() {
   const extensionContext = useContext<ExtensionContextData>(ExtensionContext)
   const sdk = extensionContext.coreSDK
   const { use_model, setUseModel, current_tables, selected_query, setSelectedQuery, setWrittenSql, written_sql } = useContext(SqlContext)
   const { setAppParams } = useContext(AppContext)
-
 
   const [selectedDateRange, setSelectedDateRange] = useState({
     from: new Date(),
@@ -38,16 +38,6 @@ export function SqlQueries() {
 
   const todaysDate = new Date()
   const todaysDateSqlFormat = todaysDate.toISOString().split('T')[0];
-
-  /**
-   *   const dropdownSqlQueries = [{
-    label: "Total views",
-    value: `SELECT wiki, SUM(views) 
-    FROM lookerdata.bq_showcase.wikipedia_v3_partition
-    WHERE DATE(datehour) = "2019-01-01" GROUP BY 1 LIMIT 1000`,
-    // lid: 205
-  }];
-   */
 
   const dropdownSqlQueries = [{
     label: "Total views",
@@ -66,34 +56,22 @@ export function SqlQueries() {
   }, []);
 
   useEffect(() => {
-    console.log('useEffect selected_query', selected_query)
+    // console.log('useEffect selected_query', selected_query)
     setWrittenSql(selected_query ? { "partitioned": selected_query, "non-partitioned": selected_query.replace("partition", "non_partition") } : {})
   }, [selected_query])
 
   useEffect(() => {
-    console.log('useEffect written_sql', written_sql)
+    // console.log('useEffect written_sql', written_sql)
 
   }, [written_sql])
 
   useEffect(() => {
-    console.log('useEffect selectedDateRange', selectedDateRange)
+    // console.log('useEffect selectedDateRange', selectedDateRange)
     if (selected_query) {
-      let selectedQueryCopy = selected_query;
-      let whereClause = selectedQueryCopy.substring(selectedQueryCopy.indexOf("WHERE"), selectedQueryCopy.indexOf("GROUP BY"))
-
-      if (DateDiff.inDays(selectedDateRange.from, selectedDateRange.to) > 1) {
-        let newWhereClause = `WHERE DATE(datehour) BETWEEN "${selectedDateRange.from.toISOString().split('T')[0]}" AND "${selectedDateRange.to.toISOString().split('T')[0]}" `;
-        selectedQueryCopy = selectedQueryCopy.replace(whereClause, newWhereClause)
-
-      } else {
-        let newWhereClause = `WHERE DATE(datehour) = "${selectedDateRange.to.toISOString().split('T')[0]}" `
-        selectedQueryCopy = selectedQueryCopy.replace(whereClause, newWhereClause)
-      }
-      setSelectedQuery(selectedQueryCopy)
+      let modifiedQuery = queryDateRangeHelper(selected_query, selectedDateRange);
+      setSelectedQuery(modifiedQuery);
     }
   }, [selectedDateRange])
-
-  // console.log({selectedDateRange} )
 
 
   return (
@@ -101,25 +79,11 @@ export function SqlQueries() {
       <Select
         options={dropdownSqlQueries}
         onChange={(value) => {
-          // console.log({ value })
-
           let SqlQueryObj = find(dropdownSqlQueries, { value: value })
-
-          let whereClause = SqlQueryObj?.value.substring(SqlQueryObj.value.indexOf("WHERE"), SqlQueryObj.value.indexOf("GROUP BY"))
-
-          if (DateDiff.inDays(selectedDateRange.from, selectedDateRange.to) > 1) {
-            let newWhereClause = `WHERE DATE(datehour) BETWEEN "${selectedDateRange.from.toISOString().split('T')[0]}" AND "${selectedDateRange.to.toISOString().split('T')[0]}" `;
-            SqlQueryObj.value = SqlQueryObj?.value.replace(whereClause, newWhereClause)
-
-          } else {
-            let newWhereClause = `WHERE DATE(datehour) = "${selectedDateRange.to.toISOString().split('T')[0]}" `
-            SqlQueryObj.value = SqlQueryObj?.value.replace(whereClause, newWhereClause)
-          }
-
-          setSelectedQuery(SqlQueryObj.value)
+          let modifiedQuery = queryDateRangeHelper(SqlQueryObj.value, selectedDateRange);
+          setSelectedQuery(modifiedQuery);
+          setSelectedQuery(modifiedQuery)
           setAppParams({ lid: SqlQueryObj.lid })
-
-
         }}
       />
       <Popover
@@ -133,58 +97,21 @@ export function SqlQueries() {
                  * Need to add logic to prevent date change here
                  */
                 setSelectedDateRange(value)
-              }}
-            // min={}
-            // max={"2019-09-01"}
+              }
+              }
             />
-          </Box>
+          </Box >
         }
       >
         <Button>
           <DateFormat>{selectedDateRange.from}</DateFormat> &mdash;
         <DateFormat>{selectedDateRange.to}</DateFormat>
         </Button>
-      </Popover>
+      </Popover >
     </Grid >
   );
 }
 
-// function DateRange() {
-//   const [selectedDateRange, setSelectedDateRange] = useState({
-//     from: new Date(),
-//     to: new Date(),
-//   })
-//   return (
-//     <Popover
-//       content={
-//         <Box p="small">
-//           <InputDateRange
-//             defaultValue={selectedDateRange}
-//             onChange={setSelectedDateRange}
-//           />
-//         </Box>
-//       }
-//     >
-//       <Button>
-//         <DateFormat>{selectedDateRange.from}</DateFormat> &mdash;
-//         <DateFormat>{selectedDateRange.to}</DateFormat>
-//       </Button>
-//     </Popover>
-//   )
-// }
-
-// function parseDate(str) {
-//   console.log("parseDate")
-//   console.log({ str })
-//   var mdy = str.split('/');
-//   return new Date(mdy[2], mdy[0] - 1, mdy[1]);
-// }
-
-// function dateDiff(first, second) {
-//   // Take the difference between the dates and divide by milliseconds per day.
-//   // Round to nearest whole number to deal with DST.
-//   return Math.round((second - first) / (1000 * 60 * 60 * 24));
-// }
 
 const DateDiff = {
 
@@ -214,4 +141,20 @@ const DateDiff = {
   inYears: function (d1, d2) {
     return d2.getFullYear() - d1.getFullYear();
   }
+}
+
+function queryDateRangeHelper(selectedQuery, selectedDateRange) {
+
+  let whereClause = selectedQuery.substring(selectedQuery.indexOf("WHERE"), selectedQuery.indexOf("GROUP BY"))
+
+  if (DateDiff.inDays(selectedDateRange.from, selectedDateRange.to) > 1) {
+    let newWhereClause = `WHERE DATE(datehour) BETWEEN "${selectedDateRange.from.toISOString().split('T')[0]}" AND "${selectedDateRange.to.toISOString().split('T')[0]}" `;
+    selectedQuery = selectedQuery.replace(whereClause, newWhereClause)
+
+  } else {
+    let newWhereClause = `WHERE DATE(datehour) = "${selectedDateRange.to.toISOString().split('T')[0]}" `
+    selectedQuery = selectedQuery.replace(whereClause, newWhereClause)
+  }
+  // setSelectedQuery(selectedQueryCopy)
+  return selectedQuery
 }
